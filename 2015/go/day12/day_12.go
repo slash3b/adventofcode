d50b6167719315a8504f44fccf9eb9c4854a8b7d
Ilya Tribusean <slash3b@gmail.com>
2021-05-13 17:14:13 +0300

    day12 part 1

---
+++ 2015/go/day12/day_12.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	readAndPrepare("/home/slash3b/Projects/aoc/2015/go/day12/input")
+}
+
+func readAndPrepare(s string) {
+	res, err := ioutil.ReadFile(s)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println("DEBUG: ", string(res))
+
+	ints := regexp.MustCompile(`\-?\d+`).FindAllString(string(res), -1)
+	fmt.Println(ints)
+
+	var sum int
+	for _, value := range ints {
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			panic(value)
+		}
+		sum += intValue
+	}
+
+	fmt.Println(sum)
+
+}
