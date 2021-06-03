9912a0e113a465ebb2e33f969abc8f6fef093841
Ilya Tribusean <slash3b@gmail.com>
2021-06-03 10:00:57 +0300

    I'm never going to solve this crap

--- 2015/go/day20/day20_test.go
+++ 2015/go/day20/day20_test.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestFindHouseByPresents(t *testing.T) {
-	ans := findHouseByPresentsNum(10)
-	if ans != 1 {
-		t.Error("incorrect")
-	}
 
-	presents := []int{10, 30, 40, 70, 60, 120, 80, 150, 130}
+	getFactorsSum(10)
 
-	for _, value := range presents {
-		ans = findHouseByPresentsNum(value)
-		fmt.Println(ans)
-	}
+	// 170 or 1, 2, 5, 10
+	//fmt.Println(getFactorsSum(9))  // 170 or 1, 3, 9
+	//fmt.Println(getFactorsSum(8)) // 170 or 1, 3, 9
+	//fmt.Println(getFactorsSum(7)) // 80 or 1, 3, 9
+
+	//getFactorsSum(2)
+	//getFactorsSum(3)
+	//getFactorsSum(4)
+
+	//for i := 1; i < 10; i++ {
+	//	fmt.Println(getPrimeFactors(10))
+	//}
 
 }
