9fa794ec9cb73365a77a3e0a807f2348d39bdd4b
Ilya Tribusean <slash3b@gmail.com>
2022-12-04 12:40:24 +0200

    day2 done

--- 2022/day2/day2.go
+++ 2022/day2/day2.go
@@ -34,18 +34,8 @@ func main() {
 	}
 }
 
-// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
 // A for Rock, B for Paper, and C for Scissors
 // X for Rock, Y for Paper, and Z for Scissors
-var m = map[byte]int{
-	'A': 1,
-	'B': 2,
-	'C': 3,
-	//
-	'X': 1,
-	'Y': 2,
-	'Z': 3,
-}
 
 var (
 	loss = 0
@@ -53,34 +43,68 @@ var (
 	win  = 6
 )
 
-func result(input string) int {
-	p1 := m[input[0]]
-	p2 := m[input[2]]
+// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
+var m1 = map[string]int{
+	"A X": 1 + draw, // Rock == Rock
+	"A Y": 2 + win,  // Rock < Paper
+	"A Z": 3 + loss, // Rock > Scissors
+	//
+	"B X": 1 + loss, // Paper > Rock
+	"B Y": 2 + draw, // Paper == Paper
+	"B Z": 3 + win,  // Paper < Scissors
+	//
+	"C X": 1 + win,  // Scissors < Rock
+	"C Y": 2 + loss, // Scissors > Paper
+	"C Z": 3 + draw, // Scissors == Scissors
+}
 
-	if p1 == p2 {
-		fmt.Printf("DRAW %s %s: %d %d: res %d \n", string(input[0]), string(input[2]), p1, p2, draw+p2)
-		return draw + p2
-	}
+// 'A' Rock
+// 'B' Paper
+// 'C' Scissors
+// 'X' Rock
+// 'Y' Paper
+// 'Z' Scissors
 
-	if p1 > p2 {
-		fmt.Printf("LOSS: %s %s: %d %d: res %d \n", string(input[0]), string(input[2]), p1, p2, loss+p2)
-		return loss + p2
-	}
-
-	fmt.Printf("WIN: %s %s: %d %d: res %d \n", string(input[0]), string(input[2]), p1, p2, win+p2)
-	return win + p2
+// X means you need to loss, Y means you need to end the round in a draw, and Z means you need to win
+// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
+// 1 for Rock, 2 for Paper, and 3 for Scissors
+var m2 = map[string]int{
+	"A X": 3 + loss, // Rock > Scissors
+	"A Y": 1 + draw, // Rock == Rock
+	"A Z": 2 + win,  // Rock < Paper
+	//
+	"B X": 1 + loss, // Paper > Rock
+	"B Y": 2 + draw, // Paper == Paper
+	"B Z": 3 + win,  // Paper < Scissors
+	//
+	"C X": 2 + loss, // Scissors > Paper
+	"C Y": 3 + draw, // Scissors == Scissors
+	"C Z": 1 + win,  // Scissors < Rock
 }
 
 func Part1(lines []string) int {
-	fmt.Printf("%v\n\n", m)
 	res := 0
 	for _, v := range lines {
-		res += result(v)
+		mv, ok := m1[v]
+		if !ok {
+			panic(fmt.Sprintf("unknown key %s", v))
+		}
+
+		res += mv
 	}
 
 	return res
 }
 
 func Part2(lines []string) int {
-	return 0
+	res := 0
+	for _, v := range lines {
+		mv, ok := m2[v]
+		if !ok {
+			panic(fmt.Sprintf("unknown key %s", v))
+		}
+		res += mv
+	}
+
+	return res
 }
