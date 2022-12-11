9f1172771c7b65fd59abc02748a328c9d0ded3d0
Ilya Tribusean <slash3b@gmail.com>
2022-12-11 23:31:32 +0200

    fuck yes, was thinking about this approach and then started to read @antirez solution and then realized I was right all along and made it :dance:

--- 2022/day6/day6.go
+++ 2022/day6/day6.go
@@ -52,6 +52,66 @@ func Part1(lines []string) int {
 	return 0
 }
 
+type Detector struct {
+	DupN int
+	m    map[rune]int
+	buf  []rune
+}
+
+func NewDetector() *Detector {
+	m := make(map[rune]int)
+
+	return &Detector{
+		m: m,
+	}
+}
+
+func (d *Detector) AddAndCheck(r rune) bool {
+	// fmt.Println("dup:", d.DupN, "buf state:", string(d.buf))
+
+	d.m[r]++
+	if d.m[r] > 1 {
+		d.DupN++
+	}
+
+	d.buf = append(d.buf, r)
+
+	if len(d.buf) > 14 {
+
+		headPop := d.buf[0]
+		d.buf = d.buf[1:]
+
+		if d.m[headPop] > 1 {
+			d.DupN--
+		}
+		d.m[headPop]--
+
+		// res := d.DupN == 0
+		// if res {
+		// 	fmt.Println("MAP content")
+		// 	for k, v := range d.m {
+		// 		fmt.Printf("k: %s, v: %d \n", string(k), v)
+		// 	}
+
+		// }
+		return d.DupN == 0
+	}
+
+	return false
+}
+
 func Part2(lines []string) int {
+
+	for _, input := range lines {
+		fmt.Println("-------------------", input)
+		dt := NewDetector()
+		for i, v := range input {
+			if dt.AddAndCheck(v) {
+				fmt.Println("line:", input, "answer:", i+1)
+				break
+			}
+		}
+	}
+
 	return 0
 }
