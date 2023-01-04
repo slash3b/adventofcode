480b3cf383463c8b214c7cbafe7bd68c24f5b562
Ilya Tribusean <slash3b@gmail.com>
2023-01-04 17:40:54 +0200

    day 8 part 1

--- 2022/day8/day8.go
+++ 2022/day8/day8.go
@@ -4,6 +4,7 @@ import (
 	"adventofcode/util"
 	"flag"
 	"fmt"
+	"strconv"
 )
 
 var test bool
@@ -23,20 +24,98 @@ func main() {
 	}
 
 	lines := util.FileToStringSlice(inputType)
+
+	input := make([][]int, 0)
+	for _, v := range lines {
+		var row []int
+		for k, _ := range v {
+			val, err := strconv.Atoi(string(v[k]))
+			if err != nil {
+				panic(err)
+			}
+			row = append(row, val)
+		}
+		input = append(input, row)
+	}
+
 	switch p {
 	case 1:
-		fmt.Println(Part1(lines))
+		fmt.Println(Part1(input))
 	case 2:
-		fmt.Println(Part2(lines))
+		fmt.Println(Part2(input))
 	default:
 		panic("wrong part number given")
 	}
 }
 
-func Part1(lines []string) int {
-	return 0
+func Part1(input [][]int) int {
+	init := len(input)*2 + len(input[0])*2 - 4
+
+	colLen := len(input)
+	rowLen := len(input[0])
+
+	var other int
+	for y := 1; y < colLen-1; y++ {
+		for x := 1; x < rowLen-1; x++ {
+			if isVisible(y, x, input) {
+				other++
+			}
+		}
+	}
+	fmt.Println("INIT", init)
+	fmt.Println("OTHER", other)
+
+	return init + other
 }
 
-func Part2(lines []string) int {
+func isVisible(y, x int, input [][]int) bool {
+	tree := input[y][x]
+	rowLen := len(input[0])
+
+	// right
+	right := true
+	for i := x + 1; i < rowLen; i++ {
+		if input[y][i] >= tree {
+			right = false
+			break
+		}
+	}
+
+	// left
+	left := true
+	for i := x - 1; i >= 0; i-- {
+		if input[y][i] >= tree {
+			left = false
+			break
+		}
+	}
+
+	// up
+	up := true
+	for i := y - 1; i >= 0; i-- {
+		if input[i][x] >= tree {
+			up = false
+			break
+		}
+	}
+
+	// down
+	colLen := len(input)
+	down := true
+	for i := y + 1; i < colLen; i++ {
+		if input[i][x] >= tree {
+			down = false
+			break
+		}
+	}
+
+	f := right || left || up || down
+
+	fmt.Println("Tree:", tree, "res: ", f, "\n", "right:", right, "left:", left, "top:", up, "bottom:", down)
+
+	return f
+}
+
+func Part2(input [][]int) int {
 	return 0
 }
