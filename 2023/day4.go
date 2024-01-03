e1079d86e83c14efe40e45c87aad32b8533abf23
Ilya Tribusean <slash3b@gmail.com>
2024-01-03 12:57:55 +0200

    day4 part 2

--- 2023/day4.go
+++ 2023/day4.go
@@ -37,6 +37,47 @@ func main() {
         }
     }
 
+    // part 2
+
+    type cardstat struct {
+        num int // total amount of cards
+        wins int // this card's wind, hence copies of cards
+    }
+
+
+    cards := make([]cardstat, len(lines))
+
+    for k, line := range lines {
+        win, nums := game(line)
+
+        wins := 0
+        for _, v := range nums {
+            _, ok := win[v]
+            if ok {
+                wins++
+            }
+        }
+
+        cards[k] = cardstat{
+            num: 1,
+            wins: wins,
+        }
+    }
+
+    for k,v := range cards {
+        // for each card
+        for i:= v.num; i>0;i-- {
+            // makes copies if any
+            for i:= 0; i < v.wins; i++ {
+                cards[k+i+1].num++
+            }
+        }
+    }
+
+    for _,v := range cards {
+        res2 += v.num
+    }
+
 	fmt.Println(res1, res2)
 }
 
